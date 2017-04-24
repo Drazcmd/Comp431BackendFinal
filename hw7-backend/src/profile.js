783e22fa974605e5d79468d2fd688ecddce5d339
Clayton Drazner <Drazcmd@gmail.com>
2017-04-24 18:30:32 -0500

    Reorganized a bit and added in setting emails
    Refactored quite a bit - realized that set headlines, email, and zip
    basically all used the same code. maybe avatar as well? Anyways, I made
    a function that an work for all of them.
    
    About to implement setting the others using it.

--- hw7-backend/src/profile.js
+++ hw7-backend/src/profile.js
@@ -17,42 +17,6 @@ exports.setup = function(app){
     app.get('/dob', isLoggedInMiddleware, dob)
 }
 
-//Note that according to the API, in many places I'm always using this as a 
-//backup for if user isn't defined somewhere in the request.
-const user = 'cmd11test'
-exports.loggedInUser = user 
-
-//(Note that user won't change since login is stubbed atm)
-const OLDprofile = {
-    headline: 'This is my headline!',
-    email: 'cmd11test@blah.com',
-    zipcode: 12345,
-    avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/DWLeebron.jpg/220px-DWLeebron.jpg',
-    dob: 700000000000
-}
-
-//in later projects we'll be using some sort of mongo database I believe
-const databaseReplacement = {
-    'sep1': {
-        headline: 'This a fake headline for my fake Dr. Pollack entry',
-        email: 'sep1test@blah.com',
-        zipcode: 13325,
-        avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/DWLeebron.jpg/220px-DWLeebron.jpg',
-        //TODO - date of birth!
-    },
-    'sep2': {
-        headline: 'This is the fake headline of a fake twin of Dr. Pollack',
-        email: 'sep2test@blah.com',
-        zipcode: 13325,
-        avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/DWLeebron.jpg/220px-DWLeebron.jpg',
-        //TODO - date of birth!
-    }
-}
-
-//Unfortunately, there's no way to use a variable as a key here 
-//without doing mutation
-databaseReplacement[user] = OLDprofile;
-
 /*
  * Builds a function to handle qurying the mongoose db. Returns
  * a promise for the results of said query.
@@ -73,33 +37,6 @@ const buildProfileQueryHandler = (multipleUsersAllowed) => {
             : {'username': santiziedUserParam}
         return model.Profile.find(databaseFilter)
     })
-}
-
-/**
- * These three functions are needed to make implementation/choice of 
- * 'database' irrelevant 
- *
- * As far as 'field', it'll generally be the first word in the endpoint; i.e.,
- * either the only thing after the url or what's specified in the requests right
- * before a ':user' or ':users'.
-*/
-const userExists = (username) => {
-     return (username in databaseReplacement)
-}
-const accessField = (username, fieldKey) => {
-     return userExists(username) ? databaseReplacement[username][fieldKey] : null
-}
-/**
- * Usually for setProfileField the value of newData is coming from a POST or 
- * PUT, wth the new value grabbed from inside req.body.
-*/
-const setProfileField = (fieldKey, newData) => {
-     //Only valid if there's already an existing value for that fieldkey
-     const validOperation = (profile[fieldKey] && newData)
-     if (validOperation){
-          profile[fieldKey] = newData
-     }
-     return validOperation
 }
 
 /*
@@ -131,39 +68,7 @@ const headlines = (req, res) => {
         console.log('Problem with database query?:', err)
         res.sendStatus(400)
     })
-
-
 }
-
-const putHeadline = (req, res) => {
-/* todo  change 
-    model.Article(newArticle).save()
-    .then(response => {
-        console.log('database response:', response) 
-        //eventually we'll need to check if we must add an image in
-        const returnedArticle = formatArticleForAPI(response)
-
-        console.log('returned article:', returnedArticle)
-        //note that wrapping it in an array is on purpose, not a bug!
-        res.send({articles: [returnedArticle]})
-    })
-    .catch(err => {
-        console.log(err)
-        res.sendStatus(400)
-    })
-}*/
-
-     //only allowed for logged in user
-    if (!(req.body.headline)) {
-        res.sendStatus(400)
-    } else {
-
-
-
-        res.send({username: req.user, headline: profile.headline})
-    }     
-}
-
 
 /**
  * Will work for any generic profile field that uses the same format for access
@@ -171,7 +76,7 @@ const putHeadline = (req, res) => {
  *
  * Note how these do NOT allow multiple inputted user ids - it takes just one!
  */
-const getStandardProfileField = (fieldKey, requestedUser, res) => {
+const getStandardProfileField = (res, fieldKey, requestedUser) => {
     console.log('accessing this field:', fieldKey, 'for this user', requestedUser)
     const handleMongooseQuery = buildProfileQueryHandler(false);
     handleMongooseQuery(requestedUser)
@@ -206,27 +111,75 @@ const getStandardProfileField = (fieldKey, requestedUser, res) => {
 //how there are exceptions like date of birth (which is otherwise the same)
 const email = (req, res) => {
     const requestedUser = req.params.user ? req.params.user : req.userObj.username
-    getStandardProfileField('email', requestedUser, res) 
+    getStandardProfileField(res, 'email', requestedUser) 
 }
 const zipcode = (req, res) => {
     const requestedUser = req.params.user ? req.params.user : req.userObj.username
-    getStandardProfileField('zipcode', requestedUser, res) 
+    getStandardProfileField(res, 'zipcode', requestedUser) 
 }
 const dob = (req, res) => {
     const loggedInUser = req.userObj.username
-    getStandardProfileField('dob', loggedInUser, res)
+    getStandardProfileField(res, 'dob', loggedInUser)
 }
 
+
+/**
+ * Usually for setProfileField the value of newData is coming from a POST or 
+ * PUT, wth the new value grabbed from inside req.body. The fieldKey is from
+ * whichever endpoint route the HTTP request got sent to.
+*/
+const setProfileField = (res, username, fieldKey, newData) => {
+    if (!(username && fieldKey && newData)){
+        console.log('missing some necessary info to set a profile field')
+        console.log('input to function:', username, fieldKey, newData)
+        res.sendStatus(400)
+        return
+    }
+    //Sadly, it's impossible to do this cleanly without mutation :/
+    const updateValueOnDatabase = {}
+    updateValueOnDatabase[fieldKey] = newData
+
+    //First arg is the filter, second is the updated stuff, third arg specifies
+    //whether it will return the new value (after being updated) or the prior value
+    model.Profile.findOneAndUpdate(
+        {'username': username}, updateValueOnDatabase, {'new':true}
+    ).then(responseFromDatabase => {
+        console.log('response to the update:', responseFromDatabase)
+
+        //gotta do the same mutation trick again here :/
+        const responseToClient = {'username': responseFromDatabase.username}
+        responseToClient[fieldKey] = responseFromDatabase[fieldKey]
+
+        res.send(responseToClient)
+    }).catch(err => {
+        console.log('problem with updating password:', err)
+        res.sendStatus(400)
+    })
+}
+
+/**
+ * Unlike with GET headline, this one is basically the exact same as the
+ * corresponding email and zipcode PUTs
+ */
+const putHeadline = (req, res) => {
+    if (!(req.body.headline)) {
+        res.sendStatus(400)
+    } else {
+        const userObj = req.userObj
+        const newHeadline = req.body.headline
+        setProfileField(res, userObj.username, 'status', newHeadline) 
+    }
+}
 const putEmail = (req, res) => {
      //(only allowed for logged in user)
      if (!req.body.email) {
         res.sendStatus(400)
      } else {
-        setProfileField('email', req.email)
-        res.send({username: req.user, email: req.body.email})
+        const userObj = req.userObj
+        const newEmail = req.body.email
+        setProfileField(res, userObj.username, 'email', newEmail) 
      }
 }
-
 const putZipcode = (req, res) => {
      //only allowed for logged in user
      if (!req.body.zipcode) {
